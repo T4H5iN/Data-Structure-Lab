334c292729b53b3a11c34a202e32516b9463b0a0
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-02-11 21:24:02 +0600

    Create Lab3b.cpp

---
+++ LAB3/Lab3b.cpp
@@ -0,0 +1,45 @@
+// Data Structure LAB on 2/5/2024.
+// Created by mdti0 on 2/5/2024.
+// Binary Search sorted in descending order
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int i, n, q;
+    cout << "Enter array size: ";
+    cin >> n;
+    int arr[n];
+    cout << "Insert " << n << " numbers:";
+    for (i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "How many query you want to do?\n";
+    cin >> q;
+    cout << "Enter the numbers you want to find:\n";
+    for (i = 0; i < q; i++) {
+        int start = 0;
+        int end = n - 1;
+        int mid;
+        int k;
+        cin >> k;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (k == arr[mid]) {
+                cout << "Position: " << mid << endl;
+                break;
+            } else if (k > arr[mid]) {
+                end = mid - 1;
+            } else if (k < arr[mid]) {
+                start = mid + 1;
+            }
+        }
+
+        if (start > end) {
+            cout << "-1" << endl;
+        }
+    }
+    return 0;
+}
