63027fedca388131cf7b97da163a2e66f2095fcd
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-05-01 20:17:20 +0600

    Update Lab9b.cpp

--- LAB9/Lab9b.cpp
+++ LAB9/Lab9b.cpp
@@ -6,57 +6,56 @@
 
 using namespace std;
 
-void push(int Basket[], int x, int &top, int n) {
-    if (top < n) {
-        Basket[top] = x;
-    } else {
-        cout << "Basket is full. Cannot push more elements." << endl;
-    }
+void push(int Basket[], int x, int &top, int &size) {
+    Basket[top] = x;
+    top++;
+    size++;
 }
 
-bool empty(int top) {
+bool empty(int &top) {
     return top == 0;
 }
 
-void pop(int &top) {
-    if (top > 0) {
-        top--;
-    } else {
-        cout << "Basket is empty. Cannot pop." << endl;
+void pop(int &top, int &size) {
+    top--;
+    size--;
+}
+
+void traverse(int Basket[], int &top, int &size) {
+    while (!empty(top)) {
+        int top_element = Basket[top - 1];
+        cout << top_element << " ";
+        pop(top, size);
     }
+}
+
+int top(int Basket[], int &top) {
+    return Basket[top - 1];
 }
 
 int main() {
     int n, x;
     cout << "Enter the number of elements: ";
     cin >> n;
-    int Basket[n], top = 0;
+    int Basket[n], top_element = 0, size = 0;
 
     cout << "Enter the elements: ";
     for (int i = 1; i <= n; i++) {
         cin >> x;
-        push(Basket, x, top, n);
-        top++;
+        push(Basket, x, top_element, size);
+    }
+    cout << "The size of the stack is " << size << endl;
+    cout << "Top element is " << top(Basket, top_element) << endl;
+
+    if (!empty(top_element)) {
+        cout << "Basket is now full" << endl;
     }
 
-    cout << "\nTop element is " << Basket[top - 1];
+    cout << "The elements in the stack are: ";
+    traverse(Basket, top_element, size);
 
-    if (empty(top)) {
+    if (empty(top_element)) {
         cout << "\nBasket is now empty" << endl;
-    } else {
-        cout << "\nBasket is not empty" << endl;
-    }
-
-    while (!empty(top)) {
-        int top_element = Basket[top - 1];
-        cout << top_element << " ";
-        pop(top);
-    }
-
-    if (empty(top)) {
-        cout << "\nBasket is now empty" << endl;
-    } else {
-        cout << "\nBasket is not empty" << endl;
     }
 
     return 0;
