c9e2710cafcb59ef8bc3ccfe458ab8be6fd832e5
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-05-18 16:03:08 +0600

    Create Lab12b.cpp

---
+++ LAB12/Lab12b.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <pair<int,int>> G[M+1];
+    int ne;
+    cin>>ne;
+    for (int i=1;i<=ne;i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        G[u].push_back(make_pair(v,w));
+        G[v].push_back(make_pair(u,w));
+    }
+    for(int u=0;u<=M;u++){
+        int l=G[u].size();
+        for(int i=0;i<=l-1;i++){
+            cout<<"(";
+            cout<<G[u][i].first<<" ";
+        cout<<G[u][i].second<<")";
+        }
+    cout<<endl;
+    }
+}
+
