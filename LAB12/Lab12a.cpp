9f518854c78842b0c1a5ce1e71514b7ceb26ac02
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-05-18 16:03:45 +0600

    Create Lab12a.cpp

---
+++ LAB12/Lab12a.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> G[M+1];
+    int ne;
+    cin>>ne;
+    for (int i=1;i<=ne;i++){
+        int u,v;
+        cin>>u>>v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    for(int u=0;u<=M;u++){
+        int l=G[u].size();
+        for(int i=0;i<=l-1;i++){
+            cout<<G[u][i]<<" ";
+        }
+        cout<<endl;
+    }
+}
