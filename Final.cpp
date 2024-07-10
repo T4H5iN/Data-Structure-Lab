3f1000e8bc2724023604a935c36d81231e39bff8
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-07-11 00:47:35 +0600

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -1,3 +1,29 @@
+// Adjacency Matrix Directed-Weighted Graph
+
+int G[M + 1][M + 1];
+for (int u = 0; u <= M; u++) {
+    for (int v = 0; v <= M; v++) {
+        if (u == v) {
+            G[u][v] = 0;
+        } else {
+            G[u][v] = -1;
+        }
+    }
+}
+int ne;
+cin >> ne;
+for (int i = 1; i <= ne; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    G[u][v] = w;
+}
+for (int i = 0; i <= M; i++) {
+    for (int j = 0; j <= M; j++) {
+        cout << G[i][j];
+    }
+    cout << endl;
+}
+
 // Adjacency List Undirected-Weighted Graph
 
 vector<pair<int, int>> G[M + 1];
