41e2c6a55594f2d47328d7bc79dab8e7292c0033
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-07-11 00:28:08 +0600

    Create Final.cpp

---
+++ Final.cpp
@@ -0,0 +1,13 @@
+void DFS(int u) {
+    color[u] = 1;
+    stopwatch++;
+    entry[u] = stopwatch;
+    for (int i = 0; i < G[u].size(); i++) {
+        int v = G[u][i];
+        if (color[v] == 0) {
+            DFS(v);
+        }
+    }
+    stopwatch++;
+    finish[u] = stopwatch;
+}
