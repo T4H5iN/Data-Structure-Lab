1ae39be5ec164f6c3846606df1d84a56652c380f
MDTI <77932280+T4H5iN@users.noreply.github.com>
2024-07-11 00:33:07 +0600

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -1,3 +1,22 @@
+//BFS
+int src;
+cin >> src;
+queue<int> Q;
+color[src] = 1;
+Q.push(src);
+
+while (!Q.empty()) {
+    int u = Q.front();
+    for (int i = 0; i < G[u].size(); i++) {
+        int v = G[u][i];
+        if (color[v] == 0) {
+            color[v] = 1;
+            Q.push(v);
+        }
+    }Q.pop();
+}
+
+//DFS
 void DFS(int u) {
     color[u] = 1;
     stopwatch++;
